c383eb1782f3fbb13274613306a65727d3fd6c16
Arthur63 <arthur63@163.com>
2024-07-02 10:51:39 +0800

    first commit

---
+++ arena.h
@@ -0,0 +1,32 @@
+#ifndef ARENA_H
+#define ARENA_H
+
+#include <stddef.h>
+
+#define ARENA_BLOCK_SIZE 4096
+#define ARENA_ALIGN_SIZE (2 * sizeof (void *))
+
+typedef struct arena_t arena_t;
+
+struct arena_t
+{
+  void *pos;
+  size_t remain;
+  struct
+  {
+    size_t cap;
+    size_t size;
+    void **data;
+  } blocks;
+};
+
+#define ARENA_INIT                                                            \
+  (arena_t) {}
+
+void arena_free (arena_t *pool);
+
+void *arena_alloc (arena_t *pool, size_t size);
+
+void *arena_aligned_alloc (arena_t *pool, size_t size, size_t align);
+
+#endif
